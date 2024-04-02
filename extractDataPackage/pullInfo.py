a3fb8d1dd19d286795d7a173b500005f80b459ad
jmhil <jmhil@DESKTOP-3IA2IBA.wifi.uc.edu>
2024-04-02 15:46:27 -0400

    Wan Commit

--- extractDataPackage/pullInfo.py
+++ extractDataPackage/pullInfo.py
@@ -1,3 +1,15 @@
+#pullInfro.py
+# Name: Cian Roopnarine, JaJuan Hill, Nosagie Sherman
+# email: roopnacn@mail.uc.edu, hill4ju@mail.uc.edu, shermani@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 3/28/24
+# Course/Section: IS 4010 Section 001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: We collaborated with our group members to execute an API using an URL.
+
+# Brief Description of what this module does: This module pulls the needed information from the extracted data
+# Citations:
+# Anything else that's relevant:
 
 def extract_info (dict):
     new_dict = dict['results']
--- mainPackage/main.py
+++ mainPackage/main.py
@@ -1,4 +1,15 @@
 #main.py
+# Name: Cian Roopnarine, JaJuan Hill, Nosagie Sherman
+# email: roopnacn@mail.uc.edu, hill4ju@mail.uc.edu, shermani@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 3/28/24
+# Course/Section: IS 4010 Section 001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: We collaborated with our group members to execute an API using an URL.
+
+# Brief Description of what this module does: this is the main, it runs the function
+# Citations:
+# Anything else that's relevant:
 
 import requests
 import json
